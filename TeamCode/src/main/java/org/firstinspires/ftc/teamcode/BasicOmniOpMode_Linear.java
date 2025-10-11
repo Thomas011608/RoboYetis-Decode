f65ed40bb4dae79671dc80948112cf0f430d38c8
Aspen-FTC-Robotics <aspenrobotics@gmail.com>
2025-10-11 12:32:27 -0600

    test remove

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOmniOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOmniOpMode_Linear.java
@@ -28,8 +28,7 @@
  */
 
 package org.firstinspires.ftc.teamcode;
-//test
-//test2
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
