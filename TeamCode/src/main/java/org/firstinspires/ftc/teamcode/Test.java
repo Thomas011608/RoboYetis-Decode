9eb11586375a8a366904ebb94909f08ef2868dd1
Thomas <thomas011608@gmail.com>
2025-10-12 13:55:32 -0600

    changed opmode name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -35,7 +35,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
-@TeleOp(name="Basic: Omni Linear OpMode", group="Linear OpMode")
+@TeleOp(name="test motor", group="Linear OpMode")
 public class Test extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
