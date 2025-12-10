380d3292817c8a5be902c398b22ba95102b82ec6
Thomas011608 <Thomas011608@gmail.com>
2025-12-09 17:26:07 -0700

    Added the code for the sorter with the color sensor and gate servo arms.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionTeleOp.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 
+import android.graphics.Color;
+
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -8,6 +10,8 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -25,20 +29,28 @@ public class CompetitionTeleOp extends LinearOpMode {
     private CRServo greenFeeder = null;
     private CRServo purpleFeeder = null;
     private DcMotor intake = null;
+    private Servo purpleGate = null;
+    private Servo greenGate = null;
+    NormalizedColorSensor colorSensor;
 
 
     ElapsedTime feederTimer = new ElapsedTime();
     ElapsedTime intakeTimer = new ElapsedTime();
+    ElapsedTime sortTimer = new ElapsedTime();
     final double INTAKE_TIME_SECONDS = 1.0;
+    final double SORT_TIME_SECONDS = 0.5;
     final double FEED_TIME_SECONDS = 0.4; //The feeder servos run this long when a shot is requested.
     final double MAX_SPEED = 1.0; //We send this power to the servos when we want them to stop.
     final double HOLD_SPEED = 0.6;
     final double STOP_SPEED = 0.0;
+    final double CLOSE = 0.0;
+    final double OPEN = 1.0;
     final double LAUNCHER_TARGET_VELOCITY_FAST = 850;
     final double LAUNCHER_MIN_VELOCITY_FAST = 800;
     final double LAUNCHER_TARGET_VELOCITY_SLOW = 675;
     final double LAUNCHER_MIN_VELOCITY_SLOW = 625;
     final double DRIVING_SPEED_MULTIPLIER = 0.8;
+    final float GAIN = 12;
     private enum LaunchState {
         IDLE,
         SPIN_UP_FAST,
@@ -51,13 +63,20 @@ public class CompetitionTeleOp extends LinearOpMode {
         INTAKE,
         HOLD,
     }
+    private enum SortState {
+        IDLE,
+        OPEN,
+        WAIT,
+    }
     private LaunchState launchState;
     private IntakeState intakeState;
+    private SortState sortState;
 
     @Override
     public void runOpMode() {
         launchState = LaunchState.IDLE;
         intakeState = IntakeState.IDLE;
+        sortState = SortState.IDLE;
 
         // HEADER: Driving Motor Definitions
         frontLeftDrive = hardwareMap.get(DcMotor.class, "front_left_drive");
@@ -93,11 +112,20 @@ public class CompetitionTeleOp extends LinearOpMode {
         greenFeeder.setDirection(CRServo.Direction.FORWARD);
         purpleFeeder.setDirection(CRServo.Direction.REVERSE);
 
+        // HEADER: Gate Servo Definitions
+        greenGate = hardwareMap.get(Servo.class,"green_gate");
+        purpleGate = hardwareMap.get(Servo.class,"purple_gate");
+        greenGate.setPosition(CLOSE);
+        purpleGate.setPosition(CLOSE);
+
         // HEADER: Intake Motor Definitions
         intake = hardwareMap.get(DcMotor.class,"intake");
         intake.setDirection(DcMotor.Direction.FORWARD);
         intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        // HEADER: Color Sensor Definitions
+        colorSensor = hardwareMap.get(NormalizedColorSensor.class, "sensor_color");
+        final float[] hsvValues = new float[3];
 
         // Wait for the game to start (driver presses START)
         telemetry.addData("Status", "Initialized");
@@ -108,8 +136,8 @@ public class CompetitionTeleOp extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+            // HEADER: defining math for driving using mecanum wheels
             double max;
-
             // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
             double axial = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
             double lateral = gamepad1.left_stick_x;
@@ -121,13 +149,11 @@ public class CompetitionTeleOp extends LinearOpMode {
             double frontRightPower = axial - lateral - yaw;
             double backLeftPower = axial - lateral + yaw;
             double backRightPower = axial + lateral - yaw;
-
             // Normalize the values so no wheel power exceeds 100%
             // This ensures that the robot maintains the desired motion.
             max = Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower));
             max = Math.max(max, Math.abs(backLeftPower));
             max = Math.max(max, Math.abs(backRightPower));
-
             if (max > 1.0) {
                 frontLeftPower /= max;
                 frontRightPower /= max;
@@ -135,18 +161,25 @@ public class CompetitionTeleOp extends LinearOpMode {
                 backRightPower /= max;
             }
 
-
-
             // Send calculated power to wheels
             frontLeftDrive.setPower(frontLeftPower*DRIVING_SPEED_MULTIPLIER);
             frontRightDrive.setPower(frontRightPower*DRIVING_SPEED_MULTIPLIER);
             backLeftDrive.setPower(backLeftPower*DRIVING_SPEED_MULTIPLIER);
             backRightDrive.setPower(backRightPower*DRIVING_SPEED_MULTIPLIER);
 
+            // HEADER: Set values fo the color sensor and set is up to get data
+            colorSensor.setGain(GAIN);
+            NormalizedRGBA colors = colorSensor.getNormalizedColors();
+            Color.colorToHSV(colors.toColor(), hsvValues);
+
+            // HEADER: Call various functions used for sorting, intake and launching
+            sort(colors.red, colors.green, colors.blue);
+
             intakeBall(gamepad2.dpadUpWasPressed(), gamepad2.dpadDownWasPressed());
 
             launch(gamepad2.xWasPressed(), gamepad2.aWasPressed(), gamepad2.bWasPressed(), gamepad2.yWasPressed());
 
+            // HEADER: Use telemetry to print any desired information to the driver hub
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
 
@@ -154,6 +187,51 @@ public class CompetitionTeleOp extends LinearOpMode {
             telemetry.addData("motorSpeed", launcher.getVelocity());
 
             telemetry.update();
+        }
+    }
+    /*void sort(double red, double green, double blue){
+        double max;
+        max = red+green+blue;
+        if (red > 0.2 && red>green && blue>green && max<2.25) {
+            telemetry.addData("Color","Purple");
+            purpleGate.setPosition(OPEN);
+        }
+        else if (green>0.2 && green>red && green>blue && max<2.25) {
+            telemetry.addData("Color","Green");
+            greenGate.setPosition(OPEN);
+        }
+    }*/
+    void sort(double red, double green, double blue){
+        double max;
+        switch (sortState) {
+            case IDLE:
+                max = red+green+blue;
+                if (max>0.3 && max<2.25){
+                    sortState = SortState.OPEN;
+                }
+                break;
+            case OPEN:
+                max = red+green+blue;
+                if (red > 0.2 && red>green && blue>green && max<2.25) {
+                    telemetry.addData("Color","Purple");
+                    purpleGate.setPosition(OPEN);
+                    sortTimer.reset();
+                    sortState = SortState.WAIT;
+                }
+                else if (green>0.2 && green>red && green>blue && max<2.25) {
+                    telemetry.addData("Color","Green");
+                    greenGate.setPosition(OPEN);
+                    sortTimer.reset();
+                    sortState = SortState.WAIT;
+                }
+                break;
+            case WAIT:
+                if (sortTimer.seconds() > SORT_TIME_SECONDS){
+                    purpleGate.setPosition(CLOSE);
+                    greenGate.setPosition(CLOSE);
+                    sortState = SortState.IDLE;
+                }
+                break;
         }
     }
     void intakeBall(boolean startIntake, boolean stopIntake){
